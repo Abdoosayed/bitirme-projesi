f28a8be659af2664c8dce694cb163138b512cd39
Abdoosayed <abdoo.almageed@gmail.com>
2024-03-03 16:12:33 +0300

    itemcard created

--- src/components/Itemcard.jsx
+++ src/components/Itemcard.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+
 
 function Itemcard() {
   return (
-    <div>Itemcard</div>
-  )
+    <>
+       <div className="card" style={{ width: '18rem' }}>
+      <img src="https://www.balaconbar.com/us/142/pidwebp600/33/f132846676788580850048-1.webp" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+
+    </>
+  );
 }
 
-export default Itemcard
+export default Itemcard;
